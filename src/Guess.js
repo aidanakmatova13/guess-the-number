5b211f6a694e98b5458094e2a8759e6796ad42ae
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-20 19:49:24 +0300

    fix code

--- src/Guess.js
+++ src/Guess.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 const Guess = () =>{
     const [number, setNumber] = useState(Math.floor(Math.random()*10))
     const [message, setMessage] = useState('')
-    const [freeAttempt, setFreeAttempt] = useState(3)
+    const [freeAttempts, setFreeAttempts] = useState(3)
     const [gamer, setGamer] = useState(0)
     const [comp, setComp] = useState(0)
     const [guess, setGuess] = useState('')
@@ -10,18 +10,18 @@ const Guess = () =>{
         setGuess(e.target.value)
     }
     const check = () =>{
-        setFreeAttempt(freeAttempt-1)
+        setFreeAttempts(freeAttempts-1)
     }
     useEffect(() =>{
-        if (number !== +guess && freeAttempt === 0){
+        if (number !== +guess && freeAttempts === 0){
             setMessage('You lose :(')
             setComp(comp+1)
         } else  if (number === +guess){
             setMessage('You won!!!')
             setGamer(gamer+1)
-            setFreeAttempt(0)
+            setFreeAttempts(0)
         }
-    }, [freeAttempt])
+    }, [freeAttempts])
 
     // const check = () =>{
     //     if (number === +guess){
@@ -38,7 +38,7 @@ const Guess = () =>{
     const reset = () =>{
         setNumber('')
         setMessage('')
-        setFreeAttempt(3)
+        setFreeAttempts(3)
         setGuess('')
     }
     return(
@@ -47,12 +47,12 @@ const Guess = () =>{
             <h1>Guess the number in 3 tries!</h1>
             <div>
                 <input onChange={num} type="number" placeholder='Enter the number' value={guess}/>
-                <button onClick={check} disabled={!freeAttempt}>CHECK</button>
+                <button onClick={check} disabled={!freeAttempts}>CHECK</button>
                 <button onClick={reset}>RESET</button>
             </div>
             <div className='message'>{message}</div>
                 {
-                    Boolean(freeAttempt)&&<div className='subtitle'>You have {freeAttempt} {freeAttempt === 1 ? 'attempt' : 'attempts' } left!</div>
+                    Boolean(freeAttempts)&&<div className='subtitle'>You have {freeAttempts} {freeAttempts === 1 ? 'attempt' : 'attempts' } left!</div>
                 }
             <h3>Player: {gamer}</h3>
             <h3>Computer: {comp}</h3>
