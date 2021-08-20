f39ef3c824644b83dc679d745251a18f9836f2f8
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-20 21:40:52 +0300

    fix code

--- src/Guess.js
+++ src/Guess.js
@@ -3,9 +3,11 @@ const Guess = () =>{
     const [number, setNumber] = useState(Math.floor(Math.random()*10))
     const [message, setMessage] = useState('')
     const [freeAttempts, setFreeAttempts] = useState(3)
-    const [gamer, setGamer] = useState(0)
-    const [comp, setComp] = useState(0)
+    const [gamer, setGamer] = useState( localStorage.getItem('gamer') || 0)
+    const [comp, setComp] = useState(localStorage.getItem('comp') || 0)
     const [guess, setGuess] = useState('')
+
+
     const num = (e) => {
         setGuess(e.target.value)
     }
@@ -13,13 +15,13 @@ const Guess = () =>{
         setFreeAttempts(freeAttempts-1)
     }
     useEffect(() =>{
-        if (number !== +guess && freeAttempts === 0){
+        if (number !== +guess && freeAttempts === 0 ){
             setMessage('You lose :(')
             setComp(comp+1)
         } else  if (number === +guess){
-            setMessage('You won!!!')
             setGamer(gamer+1)
-            setFreeAttempts(0)
+            setMessage('You won!!!')
+            // setFreeAttempts(0)
         }
     }, [freeAttempts])
 
@@ -28,27 +30,33 @@ const Guess = () =>{
     //         setMessage('You won!!!')
     //         setGamer(gamer+1)
     //     } else {
-    //         setFreeAttempt(freeAttempt-1)
-    //     } if (freeAttempt === 1){
+    //         setFreeAttempts(freeAttempts-1)
+    //     } if (freeAttempts === 1){
     //         setMessage('You lose :(')
     //         setComp(comp+1)
     //     }
     // }
 
     const reset = () =>{
-        setNumber('')
+        setNumber(Math.floor(Math.random()*10))
         setMessage('')
         setFreeAttempts(3)
         setGuess('')
     }
+    useEffect(() =>{
+        localStorage.setItem('comp', comp);
+        localStorage.setItem('gamer',gamer);
+    }, [gamer, comp])
+
     return(
         <>
             <div className='content'>
             <h1>Guess the number in 3 tries!</h1>
             <div>
                 <input onChange={num} type="number" placeholder='Enter the number' value={guess}/>
-                <button onClick={check} disabled={!freeAttempts}>CHECK</button>
+                <button onClick={check} disabled={!freeAttempts && guess > 10}>CHECK</button>
                 <button onClick={reset}>RESET</button>
+                <button onClick="refresh">REFRESH</button>
             </div>
             <div className='message'>{message}</div>
                 {
@@ -60,4 +68,5 @@ const Guess = () =>{
         </>
     )
 }
+
 export default Guess;
