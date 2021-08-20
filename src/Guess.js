529c4e0511b877bb3ec1c364c9ec2d3b94df4e75
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-20 18:29:57 +0300

    use useEffect

--- src/Guess.js
+++ src/Guess.js
@@ -1,25 +1,35 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 const Guess = () =>{
     const [number, setNumber] = useState(Math.floor(Math.random()*10))
     const [message, setMessage] = useState('')
-    const [guess, setGuess] = useState('')
     const [freeAttempt, setFreeAttempt] = useState(3)
     const [gamer, setGamer] = useState(0)
     const [comp, setComp] = useState(0)
+    const [guess, setGuess] = useState('')
     const num = (e) => {
         setGuess(e.target.value)
     }
+    // const check = () =>{
+    //     if (number === +guess){
+    //         setMessage('You won!!!')
+    //         setGamer(gamer+1)
+    //     } else {
+    //         setFreeAttempt(freeAttempt-1)
+    //     } if (freeAttempt === 1){
+    //         setMessage('You lose :(')
+    //         setComp(comp+1)
+    //     }
+    // }
     const check = () =>{
-        if (number.toString() === guess){
-            setMessage('You won!!!')
-            setGamer(gamer+1)
-        } else {
-            setFreeAttempt(freeAttempt-1)
-        } if (freeAttempt ===1){
-            setMessage('You lose :(')
-            setComp(comp+1)
-        }
+        setFreeAttempt(freeAttempt-1)
     }
+    useEffect(() =>{
+        if (number !== +guess && freeAttempt === 0){
+            setMessage('You lose :(')
+        } else  if (number === +guess){
+            setMessage('won!!!')
+        }
+    }, [freeAttempt])
     const reset = () =>{
         setNumber('')
         setMessage('')
@@ -32,7 +42,7 @@ const Guess = () =>{
             <h1>Guess the number in 3 tries!</h1>
             <div>
                 <input onChange={num} type="number" placeholder='Enter the number' value={guess}/>
-                <button onClick={check}>CHECK</button>
+                <button onClick={check} disabled={!freeAttempt}>CHECK</button>
                 <button onClick={reset}>RESET</button>
             </div>
             <div className='message'>{message}</div>
--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import ReactDOM from "react-dom";
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
