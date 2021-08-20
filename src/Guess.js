251edf124354ccfd6fc904a6345ee1aafb1b78a5
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-20 03:41:22 +0300

    add style

--- src/Guess.js
+++ src/Guess.js
@@ -4,16 +4,20 @@ const Guess = () =>{
     const [message, setMessage] = useState('')
     const [guess, setGuess] = useState('')
     const [freeAttempt, setFreeAttempt] = useState(3)
+    const [gamer, setGamer] = useState(0)
+    const [comp, setComp] = useState(0)
     const num = (e) => {
         setGuess(e.target.value)
     }
     const check = () =>{
         if (number.toString() === guess){
-            setMessage('Вы победили')
+            setMessage('You won!')
+            setGamer(gamer+1)
         } else {
             setFreeAttempt(freeAttempt-1)
         } if (freeAttempt ===1){
-            setMessage('Вы проиграли')
+            setMessage('You lose!')
+            setComp(comp+1)
         }
     }
     const reset = () =>{
@@ -24,14 +28,18 @@ const Guess = () =>{
     }
     return(
         <>
-            <h2>Угадай число с 3 попыток</h2>
+            <div className='content'>
+            <h1>Guess the number in 3 tries!</h1>
             <div>
-                <input onChange={num} type="number" placeholder='Введите число' value={guess}/>
+                <input onChange={num} type="number" placeholder='Enter the number' value={guess}/>
+                <button onClick={check}>CHECK</button>
+                <button onClick={reset}>RESET</button>
             </div>
-            <button onClick={check}>CHECK</button>
-            <button onClick={reset}>RESET</button>
             <div>{message}</div>
-            <div>У вас осталось {freeAttempt} попытки</div>
+            <div className='subtitle'>You have {freeAttempt} attempts left!</div>
+            <h3>Player: {gamer}</h3>
+            <h3>Computer: {comp}</h3>
+            </div>
         </>
     )
 }
